0aba07d23d1586beef91bc569c26e82785ccd48f
Billy Kirk <kirk.william.c@gmail.com>
2021-03-28 17:43:41 -0500

    Add togglePower

--- lifx.ts
+++ lifx.ts
@@ -1,11 +1,9 @@
 import { lifxClientConfig, Light, State } from "./types.ts";
 
 export class lifxClient {
-
     private config: lifxClientConfig;
 
     constructor(config: Partial<lifxClientConfig>) {
-
         const defaults = {
             version: "v1",
             url: "https://api.lifx.com/",
@@ -31,7 +29,11 @@ export class lifxClient {
         return this.config.token;
     }
 
-    private async lifxRequest(url: string, method: string, data?: Record<string, unknown>) {
+    private async lifxRequest(
+        url: string,
+        method: string,
+        data?: Record<string, unknown>,
+    ) {
         const json = JSON.stringify(data);
         const response = await fetch(this.getApiUrl() + url, {
             method: method,
@@ -48,42 +50,50 @@ export class lifxClient {
                 url: response.url,
                 status: response.status,
                 statusText: response.statusText,
-                data: json
+                data: json,
             });
         }
     }
 
     /**
-     * Gets lights belonging to the authenticated account. Filter the lights using selectors. Properties such as id, label, group and location can be used in selectors.
-     *
-     * @param selector - The selector to limit which lights are returned.
-     * @returns a promise containing an array of light(s)
-     */
+       * Gets lights belonging to the authenticated account. Filter the lights using selectors. Properties such as id, label, group and location can be used in selectors.
+       *
+       * @param selector - The selector to limit which lights are returned.
+       * @returns a promise containing an array of light(s)
+       */
     public listLights(selector: string): Promise<Light[]> {
         return this.lifxRequest(`/lights/${selector}`, "GET");
     }
 
     /**
-    * Sets the state of the lights within the selector. If you don't supply a parameter, the API will leave that value untouched.
-    * 
-    * State parameters:
-    * 
-    * `power` - The power state you want to set on the selector. `on` or `off`
-    * 
-    * `color` - The color to set the light to.
-    * 
-    * `brightness` - The brightness level. Range: `0.0 - 1.0`. Overrides any brightness set in color (if any).
-    * 
-    * `duration` - How long in seconds you want the power action to take. Range: `0.0 – 3155760000.0` (100 years). Default is 1.0 if unspecified. 
-    * 
-    * `infared` - The maximum brightness of the infrared channel. Range: `0.0 to 1.0`.
-    * 
-    * `fast` - Execute the query fast, without initial state checks and wait for no results. `true` or `false`. Default is `false` if unspecified.
-    *
+      * Sets the state of the lights within the selector. If you don't supply a parameter, the API will leave that value untouched.
+      * 
+      * State parameters:
+      * 
+      * `power` - The power state you want to set on the selector. `on` or `off`
+      * 
+      * `color` - The color to set the light to.
+      * 
+      * `brightness` - The brightness level. Range: `0.0 - 1.0`. Overrides any brightness set in color (if any).
+      * 
+      * `duration` - How long in seconds you want the power action to take. Range: `0.0 – 3155760000.0` (100 years). Default is 1.0 if unspecified. 
+      * 
+      * `infared` - The maximum brightness of the infrared channel. Range: `0.0 to 1.0`.
+      * 
+      * `fast` - Execute the query fast, without initial state checks and wait for no results. `true` or `false`. Default is `false` if unspecified.
+      *
+      * @param selector - The selector to limit which lights are controlled.
+      * @returns promise containing the response.
+      */
+    public setState(selector: string, state: Partial<State>) {
+        return this.lifxRequest(`/lights/${selector}/state`, "PUT", state);
+    }
+
+    /**
     * @param selector - The selector to limit which lights are controlled.
     * @returns promise containing the response.
     */
-    public setState(selector: string, state: Partial<State>) {
-        return this.lifxRequest(`/lights/${selector}/state`, "PUT", state);
+    public togglePower(selector: string) {
+        return this.lifxRequest(`/lights/${selector}/toggle`, "POST");
     }
 }
